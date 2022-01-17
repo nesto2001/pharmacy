0e07885509b49552e48bcf9629d74b661517e538
Nesto <tanlong6121@gmail.com>
2022-01-18 05:26:25 +0700

    update carousel & change list func

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-function changeTab(content, active) {
+function changeTab(evt, content, active) {
   // Declare all variables
   var i, tabcontent, tablinks, areactive;
 
@@ -14,15 +14,48 @@ function changeTab(content, active) {
   }
 
   // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
+  tablinks = document.getElementsByClassName("tab-title");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
 
   
-
+  evt.target.className += " active";
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(content).style.display = "block";
   document.getElementById(active).setAttribute('style', 'display: flex !important');
+  
 }
 
+$('.btn-plus, .btn-minus').on('click', function(e) {
+    const isNegative = $(e.target).closest('.btn-minus').is('.btn-minus');
+    const input = $(e.target).closest('.input-group').find('input');
+    if (input.is('input')) {
+      input[0][isNegative ? 'stepDown' : 'stepUp']()
+    }
+  })
+
+function changeList(evt, list, line){
+    var i, clist, cline, clinewrapper, cline;
+
+    clist = document.getElementsByClassName(list);
+    for (i = 0; i < clist.length; i++){
+        clist[i].className = clist[i].className.replace(" sd-l-active", "");
+    }
+
+    evt.target.className += " sd-l-active";
+
+    cline = document.getElementsByClassName(line);
+    for (i = 0; i < cline.length; i++){
+        cline[i].className = cline[i].className.replace(" selected-list", "");
+    }
+
+    clinewrapper = evt.target.closest('.col');
+
+    cline=clinewrapper.querySelector("."+line);
+
+    console.log(cline.className);
+
+    cline.className += " selected-list";
+
+}
